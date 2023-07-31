b68e3505fc1d115c66a82c3aca0e3eac2a6b7ac1
Gabriel Danaila <gabrield@GabrielDs-MacBook-Pro.local>
2023-07-31 23:24:59 +0300

    working on ring modue

--- script.js/ring.js
+++ script.js/ring.js
@@ -29,3 +29,30 @@ document.querySelectorAll(".navbar-list a").forEach((link) => {
     document.querySelector(".navbar-list").classList.remove("open");
   });
 });
+
+// ! == == == ==   adding images on smaller screen size  == == == ==
+window.onload = function () {
+  let image1 = document.querySelector(".img1");
+  let image2 = document.querySelector(".img2");
+  let image3 = document.querySelector(".img3");
+  let image4 = document.querySelector(".img4");
+
+  function resizeImage() {
+    if (window.innerWidth <= 720) {
+      image1.src = "../img/ring-img1-small.png";
+      image2.src = "../img/ring-img2-small.png";
+      image3.src = "../img/ring-img3-small.png";
+      image4.src = "../img/ring-img4-small.png";
+    } else {
+      image1.src = "../img/ring-img1.png";
+      image2.src = "../img/ring-img2.png";
+      image3.src = "../img/ring-img3.png";
+      image4.src = "../img/ring-img4.png";
+    }
+  }
+
+  window.onresize = resizeImage;
+  resizeImage();
+};
+
+// ! == == == ==   hover container  == == == ==
